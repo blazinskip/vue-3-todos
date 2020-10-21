10b28f1e203221cfdbffb8aa738c04bbdc7a7e5e
Patryk Blazinski <blazinski.patryk@gmail.com>
2020-10-21 18:44:32 +0200

    feat: 🎸 add empty todos page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import TodosPage from "@/views/TodosPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "TodosPage",
+    component: TodosPage
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
 ];
 
 const router = createRouter({
---
+++ src/views/TodosPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This is TodosPage
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage"
+});
+</script>
