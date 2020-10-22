091641bc4b65e8c53ab6b3e11e29c52d81a533e5
Patryk Blazinski <blazinski.patryk@gmail.com>
2020-10-22 06:54:39 +0200

    feat: 🎸 add adding todo in TodosForm

--- src/views/TodosPage.vue
+++ src/views/TodosPage.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <TodosForm />
+    <Todos :todos="todos" />
   </div>
 </template>
 
 <script lang="ts">
 import TodosForm from "@/components/TodosForm.vue";
-import { defineComponent } from "vue";
+import Todos from "@/components/Todos.vue";
+import { defineComponent, ref } from "vue";
+import { Todo } from "@/model/todo";
 
 export default defineComponent({
   name: "HomePage",
-  components: { TodosForm }
+  components: { TodosForm, Todos },
+  setup() {
+    const currentId = ref<number>(0);
+    const todos = ref<Todo[]>([{ name: "First Todo", id: currentId.value }]);
+
+    currentId.value = currentId.value++;
+    todos.value = [
+      ...todos.value,
+      { name: "Second Todo", id: currentId.value }
+    ];
+
+    return {
+      todos
+    };
+  }
 });
 </script>
